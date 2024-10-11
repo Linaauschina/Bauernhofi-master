106a749562e6361a4fd062e226cdb61a6491b346
lina.drewitz <cocofin404@gmail.com>
2024-10-11 12:15:34 +0200

    farm

--- src/main/java/org/example/Farm.java
+++ src/main/java/org/example/Farm.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 public class Farm {
 
-    Chicken chicken = new Chicken("Marvin", 3, 15);
+//   Chicken chicken = new Chicken("Marvin", 3, 15);
+//
+//   Cat cat = new Cat("Findus", 15, 5.5);
+//
+//   Pig pig = new Pig("Fridolin", 7, 55.8);
+//
+//   Cow cow = new Cow("Klaus", 3, 140.6);
 
-    Cat cat = new Cat("Findus", 15, 5.5);
-
-    Pig pig = new Pig("Fridolin", 7, 55.8);
-
-    Cow cow = new Cow("Klaus", 3, 140.6);
 
 
+    private List<String> animalsNameList = new ArrayList<>();
 
-    List<String> animalsNameList = new ArrayList<>();
+    private List<FarmAnimal> animalsList = new ArrayList<>();
 
-    List<FarmAnimal> animalsList = new ArrayList<>();
-
-    List<FarmAnimal> hungryAnimals = new ArrayList<>();
+    private List<FarmAnimal> hungryAnimals = new ArrayList<>();
 
     public Farm(){
         nameListAnimals();
@@ -48,6 +48,7 @@ public class Farm {
 
 
     public void addingHungryAnimals() {
+        //goes through the list of farmanimals and add the hungry ones to the hungry animals list
         for (FarmAnimal farmAnimal : animalsList) {
             if (farmAnimal.getHungry() == true) {
                 hungryAnimals.add(farmAnimal);
@@ -56,6 +57,7 @@ public class Farm {
     }
 
     public void feedingAllAnimals(){
+        //sets the hungry status for all animals to false
         for (FarmAnimal hungryAnimal : hungryAnimals) {
             hungryAnimal.setHungry(false);
         }
@@ -63,6 +65,14 @@ public class Farm {
 
     public List<String> getAnimalsNameList() {
         return animalsNameList;
+    }
+
+    public List<FarmAnimal> getAnimalsList() {
+        return  animalsList;
+    }
+
+    public List<FarmAnimal> getHungryAnimals(){
+        return hungryAnimals;
     }
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,97 +13,140 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        Farm farm = new Farm();
-
-
+       Farm farm = new Farm();
 
         Scanner scanner = new Scanner(System.in);
         String input = "";
         double inputWeight;
         int inputAge;
 
-
-
-//A Menu loop starts giving you options to type in to get different responses
-
         do {
+            //A Menu loop starts giving you options to type in to get different responses
             System.out.println("Type 1 to show a list of your farm animals. \nType 2 to interact with them! \n" +
                     "Type 3 to feed your animals.\nType 4 to create new animals.");
             input = scanner.next();
 
-            // the first menu option is a name list of all farmanimals
-            if (input.equals("1")) {
-                System.out.println(farm.animalsNameList);
-            }
 
-//second option is to interact with the farmanimals
 
-            if (input.equals("2")) {
-                do{
-//another loop to be able to return to your animals after interacting.
-
-                System.out.println("Your animals are waiting for you..! \n Type milk to milk " +
-                        "your cow. \n Type gather to gather your chickens eggs. \n Type play to play " +
-                        "with your cat. \n Type check to look after your pig.");
-
-                input = scanner.next();
-
-                switch (input) {
-                    case "milk" -> {
-                        System.out.println(farm.cow.milk());
-                    }
-                    case "gather" -> {
-                        System.out.println(farm.chicken.gatherEggs());
-                    }
-                    case "play" -> {
-                        System.out.println(farm.cat.play());
-                    }
-                    case "check" -> {
-                        System.out.println(farm.pig.checkIfSleeping());
-                    }
-                }
-                System.out.println("If you want to to stay with your animals type ''stay'' or type ''l'' to leave.");
-                    input = scanner.next();
-                }while (input.equals("stay"));
-            }
-//third option to check if the animals are hungry and to feed the animals.
-
-            if (input.equals("3")) {
-                farm.addingHungryAnimals();
-
-                if (farm.hungryAnimals.size() == 0) {
-                    System.out.println("All of your animals are happy and full, no need to feed.");
-                }
-                else {
-                    System.out.println("Some of your animals are hungry.. press 1 to feed them.");
-                    input = scanner.next();
-                    if (input.equals("1")) {
-                        farm.feedingAllAnimals();
-                        System.out.println("Good job, they have been fed!");
-                    } else {
-                        System.out.println("Please only type the given responses.");
-                    }
+            switch (input) {
+                case "1" -> {
+                    // the first menu option is a name list of all farmanimals
+                    System.out.println(farm.getAnimalsNameList());
                 }
 
 
 
+                case "2" -> {
+                    //second option is to interact with the farmanimals
+                    do {
+                        //another loop to be able to return to your animals after interacting.
+                        System.out.println("Your animals are waiting for you..! \n Type milk to milk " +
+                                "your cow. \n Type gather to gather your chickens eggs. \n Type play to play " +
+                                "with your cat. \n Type check to look after your pig.");
 
+                        input = scanner.next();
 
-            }
+                        switch (input) {
+                            case "milk" -> {
+                                System.out.println(farm.cow.milk());
+                            }
+                            case "gather" -> {
+                                System.out.println(farm.chicken.gatherEggs());
+                            }
+                            case "play" -> {
+                                System.out.println(farm.cat.play());
+                            }
+                            case "check" -> {
+                                System.out.println(farm.pig.checkIfSleeping());
+                            }
+                        }
+                        System.out.println("If you want to to stay with your animals type ''stay'' or type ''l'' to leave.");
+                        input = scanner.next();
+                    } while (input.equals("stay"));
+                }
 
-            if (input.equals("4")) {
-                System.out.println("Type 1 to add a new cow.\nType 2 to add a new pig.\nType 3 to add a new cat.\nType 4 to add a new chicken.");
-                input = scanner.next();
-                if (input.equals("1")) {
-                    System.out.println("Alright, type in the name.");
+                case "3" -> {
+                    //third option to check if the animals are hungry and to feed the animals.
+                    farm.addingHungryAnimals();
+
+                    if (farm.getAnimalsList().size() == 0) {
+                        System.out.println("All of your animals are happy and full, no need to feed.");
+                    }
+                    else {
+                        do {
+                            //loop to return to the beginning if u didnt press the right key
+                            System.out.println("Some of your animals are hungry.. press 1 to feed them.");
+                            input = scanner.next();
+                            if (input.equals("1")) {
+                                farm.feedingAllAnimals();
+                                System.out.println("Good job, they have been fed!");
+                            } else {
+                                System.out.println("Please only type the given responses.");
+                            }
+                        }while (!input.equals("1"));
+                    }
+
+                }
+
+                case "4" -> {
+                    //creating the possibility to create animals in the menu
+                    System.out.println("Type 1 to add a new cow.\nType 2 to add a new pig.\nType 3 to add a new cat.\nType 4 to add a new chicken.");
                     input = scanner.next();
-                    System.out.println("The name of the cow is " + input + ", now type the age in numbers not letters, you want it to be.");
-                    inputAge = scanner.nextInt();
-                    System.out.println("The cow " + input + " is " + inputAge + " years old. Finally, you can now type in the wanted weight.");
-                    inputWeight = scanner.nextDouble();
-                    System.out.println("Good Job, " + input + " is " + inputAge + " years old and weights " + inputWeight + ".");
-                    Cow cow1 = new Cow(input, inputAge, inputWeight);
-                    farm.getAnimalsNameList().add(cow1.getName());
+                    switch (input) {
+                        case "1" -> {
+
+
+                            System.out.println("Alright, type in the name.");
+                            input = scanner.next();
+                            System.out.println("The name of the cow is " + input + ", now type the age you want it to be in numbers not letters");
+                            inputAge = scanner.nextInt();
+                            System.out.println("The cow " + input + " is " + inputAge + " years old. Finally, you can now type in the wanted weight.");
+                            inputWeight = scanner.nextDouble();
+                            System.out.println("Good Job, " + input + " is " + inputAge + " years old and weights " + inputWeight + ".");
+                            Cow cow1 = new Cow(input, inputAge, inputWeight);
+                            //the new animal gets added to the name list
+                            farm.getAnimalsNameList().add(cow1.getName());
+                        }
+
+
+                        case "2" -> {
+                            //copied the method for all animals
+                            System.out.println("Alright, type in the name.");
+                            input = scanner.next();
+                            System.out.println("The name of the pig is " + input + ", now type the age you want it to be in numbers not letters");
+                            inputAge = scanner.nextInt();
+                            System.out.println("The pig " + input + " is " + inputAge + " years old. Finally, you can now type in the wanted weight.");
+                            inputWeight = scanner.nextDouble();
+                            System.out.println("Good Job, " + input + " is " + inputAge + " years old and weights " + inputWeight + ".");
+                            Pig pig1 = new Pig(input, inputAge, inputWeight);
+                            farm.getAnimalsNameList().add(pig1.getName());
+                        }
+
+                        case "3" -> {
+                            System.out.println("Alright, type in the name.");
+                            input = scanner.next();
+                            System.out.println("The name of the cat is " + input + ", now type the age you want it to be in numbers not letters");
+                            inputAge = scanner.nextInt();
+                            System.out.println("The cat " + input + " is " + inputAge + " years old. Finally, you can now type in the wanted weight.");
+                            inputWeight = scanner.nextDouble();
+                            System.out.println("Good Job, " + input + " is " + inputAge + " years old and weights " + inputWeight + ".");
+                            Cat cat1 = new Cat(input, inputAge, inputWeight);
+                            farm.getAnimalsNameList().add(cat1.getName());
+                        }
+
+                        case "4" -> {
+                            System.out.println("Alright, type in the name.");
+                            input = scanner.next();
+                            System.out.println("The name of the chicken is " + input + ", now type the age you want it to be in numbers not letters");
+                            inputAge = scanner.nextInt();
+                            System.out.println("The chicken " + input + " is " + inputAge + " years old. Finally, you can now type in the wanted weight.");
+                            inputWeight = scanner.nextDouble();
+                            System.out.println("Good Job, " + input + " is " + inputAge + " years old and weights " + inputWeight + ".");
+                            Chicken chicken1 = new Chicken(input, inputAge, inputWeight);
+                            farm.getAnimalsNameList().add(chicken1.getName());
+                        }
+                    }
+
                 }
             }
 
